0b0d1ed70b4938ee0ed937bb93371a4aa47c9fb2
Ian Ingram <iingram@sandiegozoo.org>
2019-06-24 13:41:13 -0700

    linted

--- ptz_camera.py
+++ ptz_camera.py
@@ -1,17 +1,18 @@
 from onvif import ONVIFCamera
 
+
 class PtzCam():
 
     def __init__(self):
-        IP="192.168.1.64"   # Camera IP address
-        PORT=80           # Port
-        USER="admin"         # Username
-        PASS="NyalaChow22"        # Password
+        IP = "192.168.1.64"   # Camera IP address
+        PORT = 80           # Port
+        USER = "admin"         # Username
+        PASS = "NyalaChow22"        # Password
 
         mycam = ONVIFCamera(IP, PORT, USER, PASS)
         # Create media service object
         media = mycam.create_media_service()
-    
+
         # Create ptz service object
         self.ptz = mycam.create_ptz_service()
 
@@ -21,17 +22,16 @@ class PtzCam():
         self.moverequest = self.ptz.create_type('ContinuousMove')
         self.moverequest.ProfileToken = media_profile.token
         if self.moverequest.Velocity is None:
-            self.moverequest.Velocity =  {'PanTilt': {'x': -1, 'y': -1},
-                                          'Zoom': {'x': 0.0}}
+            self.moverequest.Velocity = {'PanTilt': {'x': -1, 'y': -1},
+                                         'Zoom': {'x': 0.0}}
 
-        self.moverequest.Velocity =  {'PanTilt': {'x': -1, 'y': 1},
-                                      'Zoom': {'x': 0.0}}
+        self.moverequest.Velocity = {'PanTilt': {'x': -1, 'y': 1},
+                                     'Zoom': {'x': 0.0}}
 
     def move(self, x_dir, y_dir):
-        self.moverequest.Velocity =  {'PanTilt': {'x': x_dir, 'y': y_dir},
-                                      'Zoom': {'x': 0.0}}
+        self.moverequest.Velocity = {'PanTilt': {'x': x_dir, 'y': y_dir},
+                                     'Zoom': {'x': 0.0}}
         self.ptz.ContinuousMove(self.moverequest)
 
     def stop(self):
         self.ptz.Stop({'ProfileToken': self.moverequest.ProfileToken})
-
