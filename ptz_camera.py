1021aca8632710ad0874a02dcb5a59b93a770998
Ian Ingram <iingram@sandiegozoo.org>
2019-07-01 14:10:08 -0700

    adding an absolute move method.  refactored the class a bit towards this end as some of the stuff in init sort of assumed all requests would be continuos moves.  wondering whether there is any non-trivial overhead from having the two lines that are now moved into the move (and absmove) method(s) and whether it could make sense to have two separate types of request prepped in __init__ to use in the associated methods (as it was before when there was only the continuous move method).

--- ptz_camera.py
+++ ptz_camera.py
@@ -12,21 +12,33 @@ class PtzCam():
         mycam = ONVIFCamera(ip, port, user, pword)
         media = mycam.create_media_service()
         self.ptz = mycam.create_ptz_service()
-        media_profile = media.GetProfiles()[0]
+        self.media_profile = media.GetProfiles()[0]
 
-        self.moverequest = self.ptz.create_type('ContinuousMove')
-        self.moverequest.ProfileToken = media_profile.token
-        # if self.moverequest.Velocity is None:
-            # self.moverequest.Velocity = {'PanTilt': {'x': -1, 'y': -1},
-            #                              'Zoom': {'x': 0.0}}
+        # self.moverequest = self.ptz.create_type('ContinuousMove')
+        # self.moverequest.ProfileToken = media_profile.token
+        # # if self.moverequest.Velocity is None:
+        #     # self.moverequest.Velocity = {'PanTilt': {'x': -1, 'y': -1},
+        #     #                              'Zoom': {'x': 0.0}}
 
-        self.moverequest.Velocity = {'PanTilt': {'x': -1, 'y': 1},
-                                     'Zoom': {'x': 0.0}}
+        # self.moverequest.Velocity = {'PanTilt': {'x': -1, 'y': 1},
+        #                              'Zoom': {'x': 0.0}}
 
     def move(self, x_dir, y_dir):
+        self.moverequest = self.ptz.create_type('ContinuousMove')
+        self.moverequest.ProfileToken = self.media_profile.token
         self.moverequest.Velocity = {'PanTilt': {'x': x_dir, 'y': y_dir},
                                      'Zoom': {'x': 0.0}}
         self.ptz.ContinuousMove(self.moverequest)
+
+    def absmove(self, x_pos, y_pos):
+        self.moverequest = self.ptz.create_type('AbsoluteMove')
+        self.moverequest.ProfileToken = self.media_profile.token
+        if self.moverequest.Position is None:
+            self.moverequest.Position = self.ptz.GetStatus({'ProfileToken': self.media_profile.token}).Position
+            self.moverequest.Speed = self.media_profile.PTZConfiguration.DefaultPTZSpeed
+        self.moverequest.Position.PanTilt.x = x_pos
+        self.moverequest.Position.PanTilt.y = y_pos
+        self.ptz.AbsoluteMove(self.moverequest)
 
     def stop(self):
         self.ptz.Stop({'ProfileToken': self.moverequest.ProfileToken})
