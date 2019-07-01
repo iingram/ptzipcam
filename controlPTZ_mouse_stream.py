657a346151040d7d59523a7c1d823920bc9d1cb5
Ian Ingram <iingram@sandiegozoo.org>
2019-07-01 13:22:09 -0700

    extracting the UI stuff into a class in a separate module

--- controlPTZ_mouse_stream.py
+++ controlPTZ_mouse_stream.py
@@ -2,16 +2,12 @@ import cv2
 import threading
 
 from ptz_camera import PtzCam
+import ui
 
 IP = "192.168.1.64"   # Camera IP address
 PORT = 80           # Port
 USER = "admin"         # Username
 PASS = "NyalaChow22"        # Password
-
-mouseX = 250
-mouseY = 250
-
-WINDOW_NAME = 'Control PTZ Camera with mouse'
 
 frame = None
 latest_frame = None
@@ -27,17 +23,7 @@ def camera_thread_function(cap):
         with lo:
             latest_frame_return, latest_frame = cap.read()
 
-
-# callback function for mouse pointer tracking
-def getMouseCoords(event, x, y, flags, param):
-    global mouseX
-    global mouseY
-
-    if event == cv2.EVENT_MOUSEMOVE:
-        mouseX = x
-        mouseY = y
-
-
+            
 if __name__ == '__main__':
     ptzCam = PtzCam(IP, PORT, USER, PASS)
 
@@ -46,46 +32,22 @@ if __name__ == '__main__':
                                   daemon=True)
     cam_thread.start()
 
-    cv2.imshow(WINDOW_NAME, frame)
-    cv2.setMouseCallback(WINDOW_NAME, getMouseCoords)
-
-    width = frame.shape[1]
-    height = frame.shape[0]
-
-    zone = {'start': (int(.3*width), int(.3*height)),
-            'end': (int(.7*width), int(.7*height))}
+    ui = ui.UI_Handler(frame)
 
     x_dir = 0
     y_dir = 0
 
     while True:
-        # print(f'mouseX: {mouseX}, mouseY: {mouseY}')
-        # ok, frame = cap.read()
         if (latest_frame_return is not None) and (latest_frame is not None):
             frame = latest_frame.copy()
 
-        cv2.rectangle(frame, zone['start'], zone['end'], (255, 0, 0))
-        cv2.imshow(WINDOW_NAME, frame)
-        key = cv2.waitKey(10)
-
+        key = ui.update(frame)
         if key == ord('q'):
             break
 
         ptzCam.move(x_dir, y_dir)
 
-        if(mouseX < zone['start'][0]):
-            x_dir = -1
-        elif(mouseX > zone['end'][0]):
-            x_dir = 1
-        else:
-            x_dir = 0
-
-        if(mouseY < zone['start'][1]):
-            y_dir = 1
-        elif(mouseY > zone['end'][1]):
-            y_dir = -1
-        else:
-            y_dir = 0
+        x_dir, y_dir = ui.read_mouse()
 
         if x_dir == 0 and y_dir == 0:
             ptzCam.stop()
---
+++ ui.py
@@ -0,0 +1,52 @@
+import cv2
+
+mouseX = 250
+mouseY = 250
+
+# callback function for mouse pointer tracking
+def getMouseCoords(event, x, y, flags, param):
+    global mouseX
+    global mouseY
+
+    if event == cv2.EVENT_MOUSEMOVE:
+        mouseX = x
+        mouseY = y
+
+WINDOW_NAME = 'Control PTZ Camera with mouse'
+
+class UI_Handler():
+
+    def __init__(self, frame):
+        cv2.imshow(WINDOW_NAME, frame)
+        cv2.setMouseCallback(WINDOW_NAME, getMouseCoords)
+
+        width = frame.shape[1]
+        height = frame.shape[0]
+        
+        self.zone = {'start': (int(.3*width), int(.3*height)),
+                'end': (int(.7*width), int(.7*height))}
+        
+    def update(self, frame):
+        cv2.rectangle(frame, self.zone['start'], self.zone['end'], (255, 0, 0))
+        cv2.imshow(WINDOW_NAME, frame)
+        key = cv2.waitKey(10)
+
+        return key
+    
+    def read_mouse(self):
+
+        if(mouseX < self.zone['start'][0]):
+            x_dir = -1
+        elif(mouseX > self.zone['end'][0]):
+            x_dir = 1
+        else:
+            x_dir = 0
+
+        if(mouseY < self.zone['start'][1]):
+            y_dir = 1
+        elif(mouseY > self.zone['end'][1]):
+            y_dir = -1
+        else:
+            y_dir = 0
+
+        return x_dir, y_dir
