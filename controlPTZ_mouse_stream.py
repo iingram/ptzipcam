b988ed5531ad3cd6a39b72057f06c01270f6dcf2
Ian Ingram <iingram@sandiegozoo.org>
2019-07-09 12:08:30 -0700

    now can click to zoom in

--- controlPTZ_mouse_stream.py
+++ controlPTZ_mouse_stream.py
@@ -18,20 +18,23 @@ if __name__ == '__main__':
 
     x_dir = 0
     y_dir = 0
-
+    zoom_command = False
+    ptzCam.zoom_out_full()
+    
     while True:
         frame = cam.get_frame()
         key = ui.update(frame)
         if key == ord('q'):
             break
-        elif key == ord('o'):
-            ptzCam.zoom_out_full()
-        elif key == ord('i'):
+        
+        if zoom_command == 'i':
             ptzCam.zoom_in_full()
+        elif zoom_command == 'o':
+            ptzCam.zoom_out_full()
         
         ptzCam.move(x_dir, y_dir)
 
-        x_dir, y_dir = ui.read_mouse()
+        x_dir, y_dir, zoom_command = ui.read_mouse()
 
         if x_dir == 0 and y_dir == 0:
             ptzCam.stop()
--- ui.py
+++ ui.py
@@ -2,42 +2,51 @@ import cv2
 
 mouseX = 250
 mouseY = 250
+zoom_command = None
 
-# callback function for mouse pointer tracking
-def getMouseCoords(event, x, y, flags, param):
+
+# callback function for mouse ui
+def mouse_callback(event, x, y, flags, param):
     global mouseX
     global mouseY
+    global zoom_command
 
+    zoom_command = None
+    
     if event == cv2.EVENT_MOUSEMOVE:
         mouseX = x
         mouseY = y
 
-WINDOW_NAME = 'Control PTZ Camera with mouse'
+    if event == cv2.EVENT_LBUTTONDOWN:
+        zoom_command = 'i'
+    elif event == cv2.EVENT_LBUTTONUP:
+        zoom_command = 'o'
 
 class UI_Handler():
+    WINDOW_NAME = 'Control PTZ Camera with mouse'
 
     def __init__(self, frame):
-        cv2.imshow(WINDOW_NAME, frame)
-        cv2.setMouseCallback(WINDOW_NAME, getMouseCoords)
+        cv2.imshow(self.WINDOW_NAME, frame)
+        cv2.setMouseCallback(self.WINDOW_NAME, mouse_callback)
 
         width = frame.shape[1]
         height = frame.shape[0]
-        
+
         self.zone = {'start': (int(.3*width), int(.3*height)),
-                'end': (int(.7*width), int(.7*height))}
-        
+                     'end': (int(.7*width), int(.7*height))}
+
     def update(self, frame):
 
         cv2.rectangle(frame,
                       self.zone['start'],
                       self.zone['end'],
                       (255, 0, 0))
-        
-        cv2.imshow(WINDOW_NAME, frame)
+
+        cv2.imshow(self.WINDOW_NAME, frame)
         key = cv2.waitKey(10)
 
         return key
-    
+
     def read_mouse(self):
 
         if(mouseX < self.zone['start'][0]):
@@ -54,7 +63,7 @@ class UI_Handler():
         else:
             y_dir = 0
 
-        return x_dir, y_dir
+        return x_dir, y_dir, zoom_command
 
     def clean_up(self):
         cv2.destroyAllWindows()
