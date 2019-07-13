1a8bf852c146a1c143fed99d4b8b32212e3e33da
Ian Ingram <iingram@sandiegozoo.org>
2019-07-13 16:26:45 -0700

    rudimentary tracking

---
+++ detectTrackZoom.py
@@ -0,0 +1,99 @@
+#!/home/ian/.virtualenvs/ptzSpotter/bin/python
+
+import os
+import yaml
+
+from ptz_camera import PtzCam
+from camera import Camera
+import ui
+
+from zooSpotter import neuralnetwork as nn
+from zooSpotter import draw
+
+with open('configs.yaml') as f:
+    configs = yaml.load(f, Loader=yaml.SafeLoader)
+
+IP = configs['IP']
+PORT = configs['PORT']
+USER = configs['USER']        
+PASS = configs['PASS']
+
+CONF_THRESHOLD = configs['CONF_THRESHOLD']
+NMS_THRESHOLD = configs['NMS_THRESHOLD']
+INPUT_WIDTH = configs['INPUT_WIDTH']
+INPUT_HEIGHT = configs['INPUT_HEIGHT']
+
+path = '/home/ian/zooSpotter/models/'
+
+model_config =  os.path.join(path, 'yolov3-tiny.cfg')
+model_weights =  os.path.join(path, 'yolov3-tiny.weights')
+classes_file = os.path.join(path, 'coco.labels')
+classes = nn.read_classes_from_file(classes_file)
+
+if __name__ == '__main__':
+    ptzCam = PtzCam(IP, PORT, USER, PASS)
+    cam = Camera()
+
+    frame = cam.get_frame()
+    ui = ui.UI_Handler(frame)
+
+    network = nn.NeuralNetworkHandler(model_config,
+                                      model_weights,
+                                      INPUT_WIDTH,
+                                      INPUT_HEIGHT)
+
+    x_dir = 0
+    y_dir = 0
+    zoom_command = 'o'
+    ptzCam.zoom_out_full()
+    
+    while True:
+        frame = cam.get_frame()
+        outs, inferenceTime = network.infer(frame)
+        lboxes =  nn.NeuralNetworkHandler.filterBoxes(outs,
+                                                      frame,
+                                                      CONF_THRESHOLD,
+                                                      NMS_THRESHOLD)
+
+        x_err = 0
+        # highest_confidence_box = None
+        for lbox in lboxes:
+            if classes[lbox['classId']] == 'person':
+                # print(lbox['confidence'])
+                draw.labeledBox(frame, classes, lbox)
+                xc, yc = draw.box_to_coords(lbox['box'], return_kind='center')
+                x_err = frame.shape[1]/2 - xc
+                # print(frame.shape[1], xc, x_err)
+        #         if x_err < 50:
+        #             zoom_command = 'i'
+        # else:
+        #     zoom_command = 'o'
+                
+        key = ui.update(frame)
+     
+        if key == ord('q'):
+            break
+        
+        if zoom_command == 'i':
+            ptzCam.zoom_in_full()
+        elif zoom_command == 'o':
+            ptzCam.zoom_out_full()
+        
+        ptzCam.move(x_dir, y_dir)
+
+        # x_dir, y_dir, zoom_command = ui.read_mouse()
+
+        x_dir = - .01 * x_err
+        if x_dir >= 1.0:
+            x_dir = 1.0
+        if x_dir <= -1.0:
+            x_dir = -1.0
+        
+        print(x_dir)
+
+        if x_dir == 0 and y_dir == 0:
+            ptzCam.stop()
+
+    cam.release()
+    ptzCam.stop()
+    ui.clean_up()
