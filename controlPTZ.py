73b3af77d95a1b42a5d54f52e5a70d410d460a63
Ian Ingram <iingram@sandiegozoo.org>
2019-07-13 10:57:20 -0700

    more informative UI. partially linted.

--- controlPTZ.py
+++ controlPTZ.py
@@ -6,11 +6,10 @@ import numpy as np
 
 from onvif import ONVIFCamera
 
-IP="192.168.1.64"   # Camera IP address
-PORT=80           # Port
-USER="admin"         # Username
-PASS="NyalaChow22"        # Password
-
+IP = "192.168.1.64"   # Camera IP address
+PORT = 80           # Port
+USER = "admin"         # Username
+PASS = "NyalaChow22"        # Password
 
 XMAX = 1
 XMIN = -1
@@ -26,8 +25,9 @@ moverequest = None
 ptz = None
 active = False
 
-Y_DELTA = .01
-X_DELTA = .005
+Y_DELTA = .1
+X_DELTA = .05
+
 
 def do_move(ptz, request):
     # Start continuous move
@@ -37,7 +37,7 @@ def do_move(ptz, request):
     # active = True
     ptz.AbsoluteMove(request)
 
-    
+
 def checkZeroness(number):
     e = .001
 
@@ -50,20 +50,20 @@ def checkZeroness(number):
 def move_up(ptz, request):
     global y_command
 
-    #print ('move up...')
+    # print ('move up...')
     y_command += Y_DELTA
     y_command = checkZeroness(y_command)
     if y_command >= YMAX:
         y_command = YMAX
     request.Position.PanTilt.y = y_command
     # request.Position.PanTilt.y = YMAX
-    
+
     do_move(ptz, request)
 
-    
+
 def move_down(ptz, request):
     global y_command
-    #print ('move down...')
+    # print ('move down...')
     y_command -= Y_DELTA
     y_command = checkZeroness(y_command)
     if y_command <= YMIN:
@@ -73,10 +73,10 @@ def move_down(ptz, request):
 
     do_move(ptz, request)
 
-    
+
 def move_right(ptz, request):
     global x_command
-    #print ('move right...')
+    # print ('move right...')
     x_command += X_DELTA
     x_command = checkZeroness(x_command)
     if x_command >= XMAX:
@@ -85,10 +85,10 @@ def move_right(ptz, request):
     # request.Position.PanTilt.x = XMIN
     do_move(ptz, request)
 
-    
+
 def move_left(ptz, request):
     global x_command
-    #print ('move left...')
+    # print ('move left...')
     x_command -= X_DELTA
     x_command = checkZeroness(x_command)
     if x_command <= XMIN:
@@ -97,7 +97,7 @@ def move_left(ptz, request):
     # request.Position.PanTilt.x = XMAX
     do_move(ptz, request)
 
-    
+
 # def move_upleft(ptz, request):
 #     print ('move up left...')
 #     request.Position.PanTilt.x = XMIN
@@ -127,22 +127,22 @@ def move_left(ptz, request):
 
 
 def zoom_in(ptz, request):
-    #print ('zoom in...')
+    # print ('zoom in...')
     request.Position.Zoom.x = MAX_ZOOM
     do_move(ptz, request)
 
-    
+
 def zoom_out(ptz, request):
-    #print ('zoom out...')
+    # print ('zoom out...')
     request.Position.Zoom.x = MIN_ZOOM
     do_move(ptz, request)
 
-    
+
 def setup_move():
     mycam = ONVIFCamera(IP, PORT, USER, PASS)
     # Create media service object
     media = mycam.create_media_service()
-    
+
     # Create ptz service object
     global ptz
     ptz = mycam.create_ptz_service()
@@ -157,14 +157,13 @@ def setup_move():
         moverequest.Position = ptz.GetStatus({'ProfileToken': media_profile.token}).Position
         moverequest.Speed = media_profile.PTZConfiguration.DefaultPTZSpeed
 
-
     # Get range of pan and tilt
     # NOTE: X and Y are velocity vector
     # Get PTZ configuration options for getting continuous move range
     request = ptz.create_type('GetConfigurationOptions')
     request.ConfigurationToken = media_profile.PTZConfiguration.token
     ptz_configuration_options = ptz.GetConfigurationOptions(request)
-    
+
     global XMAX, XMIN, YMAX, YMIN, MAX_ZOOM, MIN_ZOOM
     XMAX = ptz_configuration_options.Spaces.ContinuousPanTiltVelocitySpace[0].XRange.Max
     XMIN = ptz_configuration_options.Spaces.ContinuousPanTiltVelocitySpace[0].XRange.Min
@@ -178,43 +177,47 @@ def setup_move():
 def readin():
     """Reading from stdin and displaying menu"""
     global moverequest, ptz
-    
+
     selection = sys.stdin.readline().strip("\n")
-    lov=[ x for x in selection.split(" ") if x != ""]
+    lov = [x for x in selection.split(" ") if x != ""]
     if lov:
-        
-        if lov[0].lower() in ["u","up"]:
-            move_up(ptz,moverequest)
-        elif lov[0].lower() in ["d","do","dow","down"]:
-            move_down(ptz,moverequest)
-        elif lov[0].lower() in ["l","le","lef","left"]:
-            move_left(ptz,moverequest)
-        elif lov[0].lower() in ["l","le","lef","left"]:
-            move_left(ptz,moverequest)
-        elif lov[0].lower() in ["r","ri","rig","righ","right"]:
-            move_right(ptz,moverequest)
+
+        if lov[0].lower() in ["u", "up"]:
+            move_up(ptz, moverequest)
+        elif lov[0].lower() in ["d", "do", "dow", "down"]:
+            move_down(ptz, moverequest)
+        elif lov[0].lower() in ["l", "le", "lef", "left"]:
+            move_left(ptz, moverequest)
+        elif lov[0].lower() in ["l", "le", "lef", "left"]:
+            move_left(ptz, moverequest)
+        elif lov[0].lower() in ["r", "ri", "rig", "righ", "right"]:
+            move_right(ptz, moverequest)
         elif lov[0].lower() in ["ul"]:
-            move_upleft(ptz,moverequest)
+            move_upleft(ptz, moverequest)
         elif lov[0].lower() in ["ur"]:
-            move_upright(ptz,moverequest)
+            move_upright(ptz, moverequest)
         elif lov[0].lower() in ["dl"]:
-            move_downleft(ptz,moverequest)
+            move_downleft(ptz, moverequest)
         elif lov[0].lower() in ["dr"]:
-            move_downright(ptz,moverequest)
+            move_downright(ptz, moverequest)
         elif lov[0].lower() in ["zi"]:
-            zoom_in(ptz,moverequest)
+            zoom_in(ptz, moverequest)
         elif lov[0].lower() in ["zo"]:
-            zoom_out(ptz,moverequest)
-        elif lov[0].lower() in ["s","st","sto","stop"]:
+            zoom_out(ptz, moverequest)
+        elif lov[0].lower() in ["s", "st", "sto", "stop"]:
             ptz.Stop({'ProfileToken': moverequest.ProfileToken})
             active = False
         else:
             print("What are you asking?\tI only know, 'up','down','left','right', 'ul' (up left), \n\t\t\t'ur' (up right), 'dl' (down left), 'dr' (down right) and 'stop'")
-         
+
     print("")
-    print("Your command: ", end='',flush=True)
-       
-            
+    print("Your command: ", end='', flush=True)
+
+
+def printUpdate():
+    print(f'Pan, Tilt is {x_command:.2f}, {y_command:.2f}')
+
+
 if __name__ == '__main__':
     # global moverequest, ptz
     setup_move()
@@ -230,33 +233,47 @@ if __name__ == '__main__':
     #     loop.remove_reader(sys.stdin)
     #     loop.close()
 
-    move_up(ptz,moverequest)
-    move_right(ptz,moverequest)
-    zoom_out(ptz,moverequest)
+    move_up(ptz, moverequest)
+    move_right(ptz, moverequest)
+    zoom_out(ptz, moverequest)
 
     key = 'd'
-    canvas = np.zeros((500,500), np.uint8)
+    canvas = np.zeros((500, 500), np.uint8)
     cv2.imshow('Control PTZ Camera', canvas)
 
+    print("Keys:\n",
+          "w: quit\n",
+          "k: up\n",
+          "i: down\n",
+          "j: left\n",
+          "l: right\n",
+          "z: zoom in (full)\n",
+          "a: zoom out (full)\n",
+    )
+    
     while True:
-        print(f'x, y is {x_command:.2f}, {y_command:.2f}')
 
-        
         key = cv2.waitKey(30)
 
         if key == ord('w'):
             break
         elif key == ord('i'):
-            move_up(ptz,moverequest)
+            move_up(ptz, moverequest)
+            printUpdate()
         elif key == ord('k'):
-            move_down(ptz,moverequest)
+            move_down(ptz, moverequest) 
+            printUpdate()
         elif key == ord('j'):
-            move_right(ptz,moverequest)
+            move_right(ptz, moverequest)
+            printUpdate()
         elif key == ord('l'):
-            move_left(ptz,moverequest)
+            move_left(ptz, moverequest)
+            printUpdate()
         elif key == ord('z'):
-            zoom_in(ptz,moverequest)
+            zoom_in(ptz, moverequest)
+            printUpdate()
         elif key == ord('a'):
-            zoom_out(ptz,moverequest)
+            zoom_out(ptz, moverequest)
+            printUpdate()
 
     cv2.destroyAllWindows()
