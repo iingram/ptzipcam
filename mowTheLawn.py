3b671b5212f2a806da577f367c70282d54790fc8
Ian Ingram <iingram@sandiegozoo.org>
2019-07-29 14:40:55 -0700

    now ranges mins and maxes in degrees and step duration is a constant

--- mowTheLawn.py
+++ mowTheLawn.py
@@ -15,12 +15,18 @@ PASS = "NyalaChow22"        # Password
 
 camera_still = False
 
-PAN_MIN = -.94
-PAN_MAX = -.5
+# PAN_MIN = -0.94
+# PAN_MAX = -0.5
+PAN_MIN = 30 # in degrees
+PAN_MAX = 179 # in degrees
 PAN_STEPS = 10 #400
 
-TILT_MIN = -.55
-TILT_MAX = 0
+STEP_DUR = 10
+
+TILT_MIN = -44 # in degrees
+TILT_MAX = 44 # in degrees
+# TILT_MIN = .7
+# TILT_MAX = .9
 TILT_STEPS = 10
 
 
@@ -28,10 +34,9 @@ def mow_the_lawn():
     global camera_still
     ptzCam = PtzCam(IP, PORT, USER, PASS)
 
-    ptzCam.absmove(-1, -1)
-    time.sleep(2)
+    ptzCam.absmove(PAN_MIN/180.0, TILT_MIN/45.0)
+    time.sleep(3)
 
-    step_dur = 2
     going_forward = True
     going_up = True
 
@@ -54,13 +59,16 @@ def mow_the_lawn():
                                             PAN_MIN,
                                             PAN_STEPS)
             for x_pos in pan_positions:
-                ptzCam.absmove(x_pos, y_pos)
-                time.sleep(step_dur)
+                ptzCam.absmove(x_pos/180.0, y_pos/45.0)
+                print(f'Moving to pan {x_pos} and tilt {y_pos}.') 
+                time.sleep(2)
                 camera_still = True
-                time.sleep(1)
+                time.sleep(2)
                 camera_still = False
+                time.sleep(STEP_DUR)
 
             going_forward = not going_forward
+
         going_up = not going_up
 
     ptzCam.stop()
