f0e39eece9a4afad8522022505fc010d3e6cf60a
Ian Ingram <iingram@sandiegozoo.org>
2019-08-07 15:05:39 -0700

    linted (mostly)

--- detectTrackZoom.py
+++ detectTrackZoom.py
@@ -18,7 +18,7 @@ with open('configs.yaml') as f:
     configs = yaml.load(f, Loader=yaml.SafeLoader)
 
 TRACKED_CLASS = configs['TRACKED_CLASS']
-    
+
 IP = configs['IP']
 PORT = configs['PORT']
 USER = configs['USER']
@@ -38,6 +38,7 @@ model_weights = os.path.join(MODEL_PATH, configs['MODEL_WEIGHTS_FILE'])
 classes_file = os.path.join(MODEL_PATH, configs['CLASS_NAMES_FILE'])
 classes = nn.read_classes_from_file(classes_file)
 
+
 def checkZeroness(number):
     e = .001
 
@@ -45,6 +46,7 @@ def checkZeroness(number):
         return 0
     else:
         return number
+
 
 if __name__ == '__main__':
     ptzCam = PtzCam(IP, PORT, USER, PASS)
@@ -67,7 +69,7 @@ if __name__ == '__main__':
                                  cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),
                                  15,
                                  (frame_width, frame_height))
-    
+
     x_dir = 0
     y_dir = 0
     zoom_command = 0
@@ -76,12 +78,12 @@ if __name__ == '__main__':
     ptzCam.absmove(INIT_POS[0], INIT_POS[1])
     # ptzCam.absmove(0, -0.5)
     time.sleep(2)
-    
+
     x_err = 0
     y_err = 0
 
     frames_since_last_acq = 0
-    
+
     while True:
         frame = cam.get_frame()
 
@@ -92,7 +94,7 @@ if __name__ == '__main__':
                                                      NMS_THRESHOLD)
 
         highest_confidence_tracked_class = 0
-        target_lbox= None
+        target_lbox = None
         for lbox in lboxes:
             if classes[lbox['classId']] in TRACKED_CLASS:
                 if lbox['confidence'] > highest_confidence_tracked_class:
@@ -102,8 +104,10 @@ if __name__ == '__main__':
         if target_lbox:
             frames_since_last_acq = 0
             draw.labeledBox(frame, classes, target_lbox)
-            xc, yc = draw.box_to_coords(target_lbox['box'], return_kind='center')
-            x, y, box_width, box_height = draw.box_to_coords(target_lbox['box'])
+            xc, yc = draw.box_to_coords(target_lbox['box'],
+                                        return_kind='center')
+            ret = draw.box_to_coords(target_lbox['box'])
+            x, y, box_width, box_height = ret
             x_err = frame_width/2 - xc
             y_err = frame_height/2 - yc
 
@@ -126,13 +130,11 @@ if __name__ == '__main__':
                 zoom_command = -1.0
             # zoom_command = -1.0
 
-        
         zoom_command = checkZeroness(zoom_command)
 
-                
         key = ui.update(frame, hud=False)
         vid_writer.write(frame.astype(np.uint8))
-        
+
         if key == ord('q'):
             break
 
@@ -156,12 +158,12 @@ if __name__ == '__main__':
             #     command = 0.0
 
             return command
-                
+
         # x_dir = calc_command(x_err, -.005)
         # y_dir = calc_command(y_err, .005)
         x_dir = calc_command(x_err, -.0008)
         y_dir = calc_command(y_err, .002)
-            
+
         # print(x_dir, y_dir, zoom_command)
 
         if x_dir == 0 and y_dir == 0:
