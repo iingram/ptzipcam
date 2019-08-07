30e641ade9fcf655bb25c1a046aa4d181a9737df
Ian Ingram <iingram@sandiegozoo.org>
2019-08-07 16:59:06 -0700

    now camera can be set for a camera on its side

--- controlPTZ_mouse_stream.py
+++ controlPTZ_mouse_stream.py
@@ -3,20 +3,27 @@
 # Need to do something in the realm of this first:
 # ffmpeg -rtsp_transport tcp -i rtsp://admin:NyalaChow22@192.168.1.64:554/Streaming/Channels/103 -b 1900k -f mpegts udp://127.0.0.1:5000
 
+import numpy as np
+
+import ui
+
 from ptz_camera import PtzCam
 from camera import Camera
-import ui
 
 IP = "192.168.1.64"   # Camera IP address
 PORT = 80           # Port
 USER = "admin"         # Username
 PASS = "NyalaChow22"        # Password
-       
+
+ROTATED = False
+
 if __name__ == '__main__':
     ptzCam = PtzCam(IP, PORT, USER, PASS)
     cam = Camera()
 
     frame = cam.get_frame()
+    if ROTATED:
+        frame = np.rot90(frame)
     window_name = 'Control PTZ Camera with mouse'
     ui = ui.UI_Handler(frame, window_name)
 
@@ -27,6 +34,8 @@ if __name__ == '__main__':
     
     while True:
         frame = cam.get_frame()
+        if ROTATED:
+            frame = np.rot90(frame)
         key = ui.update(frame)
         if key == ord('q'):
             break
@@ -35,9 +44,12 @@ if __name__ == '__main__':
             ptzCam.zoom_in_full()
         elif zoom_command == 'o':
             ptzCam.zoom_out_full()
-        
-        ptzCam.move(x_dir, y_dir)
 
+        if ROTATED: 
+            ptzCam.move(y_dir, -x_dir)
+        else:
+            ptzCam.move(x_dir, y_dir)
+                        
         x_dir, y_dir, zoom_command = ui.read_mouse()
 
         if x_dir == 0 and y_dir == 0:
