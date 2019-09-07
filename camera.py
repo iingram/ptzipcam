a91e868270e41a2251163ed80cf996b6c5d56a1c
Ian Ingram <iingram@sandiegozoo.org>
2019-09-07 06:27:18 -0700

    now constructor can take udp address of stream

--- camera.py
+++ camera.py
@@ -12,12 +12,11 @@ def camera_thread_function(cap):
         with lo:
             latest_frame_return, latest_frame = cap.read()
 
-
 class Camera():
 
-    def __init__(self):
+    def __init__(self, address='udp://127.0.0.1:5000'):
         self.frame = None
-        self.cap = cv2.VideoCapture('udp://127.0.0.1:5000', cv2.CAP_FFMPEG)
+        self.cap = cv2.VideoCapture(address, cv2.CAP_FFMPEG)
         ok, self.frame = self.cap.read()
 
         self.cam_thread = threading.Thread(target=camera_thread_function,
