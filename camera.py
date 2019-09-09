6774bdbaebd5769825d0c6cbd574b88f01a06e05
Ian Ingram <iingram@sandiegozoo.org>
2019-09-08 17:38:52 -0700

    now no use of globals so can create multiple instances of the Camera class and they'll each have their own thread for keeping up with the actual image stream.
    NOTE: am dropping the check on the return from the VideoCapture and
    the thread locking (which probably isn't necessary)

--- camera.py
+++ camera.py
@@ -1,36 +1,37 @@
 import threading
 import cv2
 
-latest_frame = None
-latest_frame_return = None
-lo = threading.Lock()
+# latest_frame = None
+# latest_frame_return = None
+# lo = threading.Lock()
 
 
-def camera_thread_function(cap):
-    global latest_frame, lo, latest_frame_return
+def camera_thread_function(cap, frame):
+    # global latest_frame, lo, latest_frame_return
     while True:
-        with lo:
-            latest_frame_return, latest_frame = cap.read()
+        # with lo:
+            # latest_frame_return, latest_frame = cap.read()
+        ok, frame[0] = cap.read()
 
 class Camera():
 
     def __init__(self, address='udp://127.0.0.1:5000'):
-        self.frame = None
+        self.frame = [None]
         self.cap = cv2.VideoCapture(address, cv2.CAP_FFMPEG)
-        ok, self.frame = self.cap.read()
+        ok, self.frame[0] = self.cap.read()
 
         self.cam_thread = threading.Thread(target=camera_thread_function,
-                                           args=(self.cap,))
+                                           args=(self.cap, self.frame))
         self.cam_thread.start()
 
     def get_frame(self):
-        if (latest_frame_return is not None) and (latest_frame is not None):
-            self.frame = latest_frame.copy()
+        # if (latest_frame_return is not None) and (latest_frame is not None):
+        #     self.frame = latest_frame.copy()
 
-        return self.frame
+        return self.frame[0]
 
     def get_resolution(self):
-        return self.frame.shape[1], self.frame.shape[0]
+        return self.frame[0].shape[1], self.frame[0].shape[0]
 
     def release(self):
         self.cap.release()
