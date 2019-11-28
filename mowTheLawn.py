7e810a7143ad1510fcfc12321deb4b43ab92e60b
Ian Ingram <iingram@sandiegozoo.org>
2019-11-28 11:20:55 -0800

    - now optionally sends frames to another computer over a socket
    - linted

--- mowTheLawn.py
+++ mowTheLawn.py
@@ -1,7 +1,10 @@
+import sys
 import time
 import threading
 import socket
 import logging
+import pickle
+import struct
 
 import cv2
 
@@ -11,13 +14,20 @@ from ptz_camera import PtzCam
 from camera import Camera
 
 IP = "192.168.1.64"   # Camera IP address
-PORT = 80           # Port
+ONVIF_PORT = 80           # Port
 USER = "admin"         # Username
 PASS = "NyalaChow22"        # Password
 
 camera_still = False
 
 HEADLESS = False
+
+if len(sys.argv) > 1:
+    CLIENT_MODE = True
+    HOST = sys.argv[1]
+    PORT = 8485
+else:
+    CLIENT_MODE = False
 
 # PAN_MIN = -0.94
 # PAN_MAX = -0.5
@@ -33,20 +43,23 @@ TILT_MAX = 44  # in degrees
 # TILT_MAX = .9
 TILT_STEPS = 10
 
+
 def convert_degrees_to_pan_command(degrees, full_range):
     if degrees > full_range:
-        logging.error('Angle higher than full range')        
+        logging.error('Angle higher than full range')
         degrees = full_range
     elif degrees < 0.0:
-        logging.error('Angle lower than zero')        
+        logging.error('Angle lower than zero')
         degrees = 0.0
-    
+
     half_range = full_range/2.0
     return (degrees - half_range)/half_range
+
 
 def convert_pan_command_to_degrees(command, full_range):
     half_range = full_range/2.0
     return command * half_range + half_range
+
 
 def mow_the_lawn():
     """Thread function for moving the camera through a "mow the lawn"
@@ -54,7 +67,7 @@ def mow_the_lawn():
     up a step, etc.
     """
     global camera_still
-    ptz_cam = PtzCam(IP, PORT, USER, PASS)
+    ptz_cam = PtzCam(IP, ONVIF_PORT, USER, PASS)
 
     pan_min = convert_degrees_to_pan_command(PAN_MIN, 350.0)
     pan_max = convert_degrees_to_pan_command(PAN_MAX, 350.0)
@@ -104,14 +117,18 @@ def mow_the_lawn():
 
 
 if __name__ == '__main__':
+    if CLIENT_MODE:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((HOST, PORT))
+        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+
     logging.basicConfig(level=logging.DEBUG, filename='log.log')
-    
+
     movement_control_thread = threading.Thread(target=mow_the_lawn,
                                                daemon=True)
     movement_control_thread.start()
 
     cam = Camera(ip=IP, user=USER, passwd=PASS)
-
     width, height = cam.get_resolution()
 
     hostname = socket.gethostname()
@@ -139,6 +156,13 @@ if __name__ == '__main__':
                     vid_writer.write(frame.astype(np.uint8))
                     print('Taking a shot.')
                     latch = False
+                    if CLIENT_MODE:
+                        result, frame_to_send = cv2.imencode('.jpg',
+                                                             frame,
+                                                             encode_param)
+                        data = pickle.dumps(frame_to_send, 0)
+                        size = len(data)
+                        sock.sendall(struct.pack(">L", size) + data)
             elif not latch:
                 latch = True
 
@@ -147,6 +171,9 @@ if __name__ == '__main__':
 
     vid_writer.release()
     cam.release()
+
+    if CLIENT_MODE:
+        sock.close()
 
     if not HEADLESS:
         cv2.destroyAllWindows()
