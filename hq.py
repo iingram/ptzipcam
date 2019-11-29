4e3d354b34fb39a68901dd650fd6bed689802c9d
Ian Ingram <iingram@sandiegozoo.org>
2019-11-29 11:34:56 -0800

    lays out image in a grid

--- hq.py
+++ hq.py
@@ -1,16 +1,21 @@
-# originally copied from
-# https://gist.github.com/kittinan/e7ecefddda5616eab2765fdb2affed1b
-
 import socket
 import struct
 import pickle
 import cv2
-# import zlib
+
+import screeninfo
+
+import numpy as np
+
+from dnntools import draw
 
 HOST = ''
 PORT = 8485
 
-window_name = "Server Window"
+window_name = "HQ"
+
+screen_width = screeninfo.get_monitors()[0].width
+screen_height = screeninfo.get_monitors()[0].height
 
 cv2.namedWindow(window_name,
                 cv2.WINDOW_NORMAL)
@@ -18,6 +23,11 @@ cv2.namedWindow(window_name,
 cv2.setWindowProperty(window_name,
                       cv2.WND_PROP_FULLSCREEN,
                       cv2.WINDOW_FULLSCREEN)
+
+canvas = np.zeros((screen_height, screen_width, 3), np.uint8)
+
+# cv2.imshow(window_name, canvas)
+# cv2.waitKey(1)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print('Socket created')
@@ -33,6 +43,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     data = b""
     header_size = struct.calcsize(">L")
     print("header_size: {}".format(header_size))
+
+    x = 0
+    y = 0
+
     while True:
         # while len(data) < header_size:
         #     print("Recv: {}".format(len(data)))
@@ -51,11 +65,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         # if no data, quit.  is there a gotcha here?
         if len(data) == 0:
             break
-        
+
         print("Done Recv: {}".format(len(data)))
-        header = data[:header_size] # get header bytes from data
-        data = data[header_size:]   # cut header bytes off the data bytearray
-        msg_size = struct.unpack(">L", header)[0] # read header for the size of payload
+        header = data[:header_size]
+        data = data[header_size:]
+        msg_size = struct.unpack(">L", header)[0]
         print("msg_size: {}".format(msg_size))
         while len(data) < msg_size:
             data += conn.recv(4096)
@@ -64,5 +78,17 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
         frame = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
         frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
-        cv2.imshow(window_name, frame)
-        cv2.waitKey(1)
+
+        draw.image_onto_image(canvas, frame, (340*x, 260*y))
+        x += 1
+        if x >= 11:
+            x = 0
+            y += 1
+
+        if y >= 8:
+            y = 0
+
+        cv2.imshow(window_name, canvas)
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
