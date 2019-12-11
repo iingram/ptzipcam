6cc290fa4d3afaf69fa4c73ae83a5be30744c6ef
Ian Ingram <iingram@sandiegozoo.org>
2019-12-11 13:30:38 -0800

    socket server in a thread so we can start doing visualization stuff in real-time, not blocked by receipt of new frames.

--- hq.py
+++ hq.py
@@ -1,3 +1,4 @@
+import threading
 import socket
 import struct
 import pickle
@@ -8,6 +9,7 @@ import screeninfo
 import numpy as np
 
 from dnntools import draw
+from viztools import visualization as viz
 
 HOST = ''
 PORT = 8485
@@ -29,66 +31,79 @@ canvas = np.zeros((screen_height, screen_width, 3), np.uint8)
 # cv2.imshow(window_name, canvas)
 # cv2.waitKey(1)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    print('Socket created')
+frame = canvas
 
-    s.bind((HOST, PORT))
-    print('Socket bind complete')
-    s.listen(10)
-    print('Socket now listening')
+pics = []
 
-    conn, addr = s.accept()
-    print('Socket connection made')
+def socket_function():
+    global pics
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        print('Socket created')
 
-    data = b""
-    header_size = struct.calcsize(">L")
-    print("header_size: {}".format(header_size))
+        s.bind((HOST, PORT))
+        print('Socket bind complete')
+        s.listen(10)
+        print('Socket now listening on port', PORT)
 
-    x = 0
-    y = 0
+        conn, addr = s.accept()
+        print('Socket connection made')
 
-    while True:
-        # while len(data) < header_size:
-        #     print("Recv: {}".format(len(data)))
-        #     data += conn.recv(4096)
+        data = b""
+        header_size = struct.calcsize(">L")
+        print("header_size: {}".format(header_size))
 
-        # i commented out the above in favor of just the one line below
-        # but i see now that the code below allows for the idea that some
-        # of the payload data gets received in that first call to recv so
-        # maybe there is some argument for doing it the way they had done
-        # it?  One hiccup (but I haven't thought through this carefully)
-        # could be that if a payload was smaller than the remainder of the
-        # 4096 bytes that could cause a problem with their implementation?
+        while True:
+            data += conn.recv(header_size)
 
-        data += conn.recv(header_size)
+            # if no data, quit.  is there a gotcha here?
+            if len(data) == 0:
+                break
 
-        # if no data, quit.  is there a gotcha here?
-        if len(data) == 0:
-            break
+            print("Done Recv: {}".format(len(data)))
+            header = data[:header_size]
+            data = data[header_size:]
+            msg_size = struct.unpack(">L", header)[0]
+            print("msg_size: {}".format(msg_size))
+            while len(data) < msg_size:
+                data += conn.recv(4096)
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
 
-        print("Done Recv: {}".format(len(data)))
-        header = data[:header_size]
-        data = data[header_size:]
-        msg_size = struct.unpack(">L", header)[0]
-        print("msg_size: {}".format(msg_size))
-        while len(data) < msg_size:
-            data += conn.recv(4096)
-        frame_data = data[:msg_size]
-        data = data[msg_size:]
+            frame = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+            frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
 
-        frame = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
-        frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+            pics.append(viz.FlyingGrowingPicBox(frame,
+                                                np.array((0,0)),
+                                                np.array((1500, 1500)),
+                                                200,
+                                                75))
 
-        draw.image_onto_image(canvas, frame, (340*x, 260*y))
-        x += 1
-        if x >= 11:
-            x = 0
-            y += 1
+            
+            
+socket_thread = threading.Thread(target=socket_function,
+                                 daemon=True)
+socket_thread.start()
 
-        if y >= 8:
-            y = 0
+x = 0
+y = 0
 
-        cv2.imshow(window_name, canvas)
-        key = cv2.waitKey(1)
-        if key == ord('q'):
-            break
+while True:
+    canvas = np.zeros((screen_height, screen_width, 3), np.uint8)
+
+    for pic in pics:
+        pic.update()
+        pic.display(canvas)
+    
+    # draw.image_onto_image(canvas, frame, (340*x, 260*y))
+    # x += 1
+    # if x >= 11:
+    #     x = 0
+    #     y += 1
+
+    # if y >= 8:
+    #     y = 0
+
+    cv2.imshow(window_name, canvas)
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
