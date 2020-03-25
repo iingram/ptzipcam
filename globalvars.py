33793d3865d9bb11051c58ed7b977a7a0bd15dcf
Ian Ingram <ianishidden@gmail.com>
2020-03-24 23:15:29 -0700

    needs to be declared global

--- globalvars.py
+++ globalvars.py
@@ -1,5 +1,6 @@
 def init():
-    global camera_still, pan_angle, tilt_angle
+    global camera_still, pan_angle, tilt_angle, grid
     camera_still = False
     pan_angle = None
     tilt_angle = None
+    grid = (None, None)
