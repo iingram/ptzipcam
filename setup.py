0249ba511074c25eb12fec96c992937743cc463d
Ian Ingram <ianishidden@gmail.com>
2021-05-27 07:19:08 -0700

    updating setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,35 @@
+import os
+import re
+
 import setuptools
 
 
+def read(filename):
+    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), filename)
+    with open(path , 'r') as f:
+      return f.read()
+
+
+def find_version(text):
+    match = re.search(r"^__version__\s*=\s*['\"](.*)['\"]\s*$", text,
+                      re.MULTILINE)
+    return match.group(1)
+
+
+DESC = "Package for controlling PTZ IP cameras that support ONVIF"
+
 setuptools.setup(
     name="ptzipcam",
+    description=DESC,
+    license="MIT"
     author="Ian Ingram",
     version="0.0.1",
     author_email="ianishidden@gmail.com",
-    description="For controlling PTZ IP cameras that support ONVIF.",
     packages=setuptools.find_packages(),
+    install_required=[
+        'pyyaml',
+        'opencv-python',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
