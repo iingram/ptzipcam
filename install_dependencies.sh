0f99c93ae76f778b423e036a66b1a1f5ced00f7c
Ian Ingram <iingram@sandiegozoo.org>
2021-10-25 17:09:11 -0700

    Update install script to check for Python 3.7
    Current versions of numpy require 3.7 or above and generally it seems
    time to make this requirement for ptzipcam as a whole.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,13 @@
+if ! command -v python3.7 &> /dev/null
+then
+    echo "Python 3.7 not found. Make sure you have 3.7 or higher."
+    echo "Aborting dependency install."
+    return 1
+else
+    echo "Python 3.7 was found."
+    echo "_____________________"
+fi
+
 sudo apt -y install libopencv-dev 
 sudo apt -y install libatlas-base-dev
 sudo apt -y install libjasper-dev
