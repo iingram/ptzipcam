5c781c0b5cd84b8ad825cb571cc52b85256bdb9e
Ian Ingram <ianishidden@gmail.com>
2023-01-05 14:54:17 -0800

    Generalize this utility

--- utilities/record_series_of_clips.sh
+++ utilities/record_series_of_clips.sh
@@ -1,4 +1,4 @@
-echo Starting HerpCam Clip Recording
+echo Starting Clip Recording
 echo Press Q to exit.
 
 trap 'kill $(jobs -p)' SIGINT SIGTERM EXIT
@@ -7,9 +7,8 @@ trap 'kill $(jobs -p)' SIGINT SIGTERM EXIT
 while true; do
     printf "Next clip\n"
     current_time=$(date "+%Y.%m.%d-%H.%M.%S")
-    # file_name=bushmasters
-    file_name=/home/ian/mang_vipers/$current_time.mp4
-    ffmpeg -i rtsp://admin:b22shm**stters@10.10.32.206:554/Streaming/Channels/102 -acodec copy -vcodec copy $file_name -nostdin  &
+    file_name=/home/username/mang_vipers/$current_time.mp4
+    ffmpeg -i rtsp://cam_username:cam_password@cam_ip:554/Streaming/Channels/102 -acodec copy -vcodec copy $file_name -nostdin  &
     vlc_pid=$!
 
     sleep 30m
