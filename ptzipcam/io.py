43fe0b9ccfc55a96f0543a1020334d83cb20e541
Ian Ingram <ianishidden@gmail.com>
2023-03-06 12:28:05 -0800

    Lint

--- ptzipcam/io.py
+++ ptzipcam/io.py
@@ -1,3 +1,9 @@
+"""Tools for recording output from camera applications
+
+At this stage, this amounts to a single class for recording stills.
+
+
+"""
 import os
 import time
 import math
@@ -6,7 +12,14 @@ from datetime import datetime
 import cv2
 
 
-class ImageStreamRecorder():
+class ImageStreamRecorder():  # pylint: disable=too-few-public-methods
+    """Handles recording images with their associate metadata
+
+    The metadata are the PTZ state when the image was captured and
+    objection detection (bounding box data, detected class, score)
+    tied to that images capture.
+
+    """
 
     def __init__(self, path):
         self.path = path
@@ -17,31 +30,56 @@ class ImageStreamRecorder():
         if not os.path.exists(self.image_path):
             os.mkdir(self.image_path)
         else:
-            print('[WARNING] Images path already exists so there could end up being \
-            images from two runs in one folder or worse images could \
-            be over written (not too likely as they have millisecond \
-            timestamps but not impossible.).  This also means that is \
-            likely a system clock is off for the folder name (also \
-            timestamped) to be duplicate')
-        self.record_file = timestamp_string + '.csv'
-        self.record_file = os.path.join(self.path, self.record_file)
-        with open(self.record_file, 'w') as f:
-            f.write('IMAGE_FILE,PAN_ANGLE,TILT_ANGLE,ZOOM_POWER,CLASS,SCORE,X,Y,W,H\n')
+            warning = ('[WARNING] Images path already exists so there could '
+                       'end up being images from two runs in one folder or '
+                       'worse images could be over-written (not too likely as '
+                       'they have millisecond timestamps but not impossible.).'
+                       ' This also means that it is likely a system clock is '
+                       'off for the folder name (also timestamped) to be '
+                       'duplicate')
+            print(warning)
+        self.record_filename = timestamp_string + '.csv'
+        self.record_filename = os.path.join(self.path, self.record_filename)
+        with open(self.record_filename, 'w', encoding='utf-8') as record_file:
+            header_line = ('IMAGE_FILE,PAN_ANGLE,TILT_ANGLE,'
+                           'ZOOM_POWER,CLASS,SCORE,X,Y,W,H\n')
+            record_file.write(header_line)
 
-    def record_image(self,
+    def record_image(self,  # pylint: disable=too-many-locals
                      image,
                      ptz_state,
                      detected_class,
                      target_lbox):
+        """Record a single image to a file and append the associated PTZ state
+        and object detection data to the recording CSV file.
+
+        Parameters
+        ----------
+
+        image :
+            The image to record
+
+        ptz_state : 3-tuple
+            Contains the pan, tilt, zoom of the camera at the time the
+            image was captured.
+
+        detected_class :
+            The name of the detected class
+
+        target_lbox : dict
+            The parameters of the labeled bounding box of the detected target.
+
+        """
 
         pan_angle, tilt_angle, zoom = ptz_state
 
         front_bit = time.strftime(self.timestamp_format)
         # maybe you should avoid a call to time and datetime and just
         # get everything from datetime. later.
-        dt = datetime.now()
+        now = datetime.now()
+        milliseconds = math.floor(now.microsecond/1000)
         front_bit = (front_bit
-                     + '_{:03d}'.format(math.floor(dt.microsecond/1000)))
+                     + f'_{milliseconds:03d}')
         image_filename = front_bit + '.jpg'
 
         # full_path = os.path.join(self.path, 'images')
@@ -54,13 +92,14 @@ class ImageStreamRecorder():
 
         if target_lbox:
             score = 100 * target_lbox['confidence']
-            x, y, w, h = target_lbox['box']
+            box = target_lbox['box']
+            box_x_coord, box_y_coord, box_width, box_height = box
         else:
             score = 0
-            x = 0
-            y = 0
-            w = 0
-            h = 0
+            box_x_coord = 0
+            box_y_coord = 0
+            box_width = 0
+            box_height = 0
 
         strg = '{},{:.2f},{:.2f},{:.2f},{},{:.1f},{},{},{},{}\n'
         record_line = strg.format(image_filename,
@@ -69,10 +108,10 @@ class ImageStreamRecorder():
                                   zoom,
                                   detected_class,
                                   score,
-                                  x,
-                                  y,
-                                  w,
-                                  h)
+                                  box_x_coord,
+                                  box_y_coord,
+                                  box_width,
+                                  box_height)
 
-        with open(self.record_file, 'a') as f:
-            f.write(record_line)
+        with open(self.record_filename, 'a', encoding='utf-8') as record_file:
+            record_file.write(record_line)
